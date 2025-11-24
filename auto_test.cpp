4d71a428eb3ab12ae052dabe91e8336f2d3a587c
Bohdan Butkevich <144889884+Daankoo@users.noreply.github.com>
2025-11-24 14:00:52 +0200

    Update auto_test.cpp

--- auto_test.cpp
+++ auto_test.cpp
@@ -1,19 +1,9 @@
 #include <iostream>
-#include "count.h"
+#include "stranger.h"
 
 using namespace std;
 
 int main() {
-    
-    int result = count(2026, 1, 1);
-    
-    if (result == 4) {
-        cout << "Test 1 Passed! Result is 4.\n";
-    } 
-    else {
-        cout << "Test 1 Failed! Expected 4, got " << result;
-        return 1;    
-    }
 
     return 0; 
 }
